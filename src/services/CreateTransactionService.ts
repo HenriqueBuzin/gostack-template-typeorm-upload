197ca82b9b8855f66bf94551efd958215655b0a6
henrique_buzin <cultura.henrique@gmail.com>
2020-12-29 15:06:06 -0300

    6 passed

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,4 +1,4 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 import { getCustomRepository, getRepository } from "typeorm";
 
@@ -26,6 +26,12 @@ class CreateTransactionService {
     const transactionsRepository = getCustomRepository(TransactionsRepository);
 
     const categoryRepository = getRepository(Category);
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (type == 'outcome' && total < value) {
+      throw new AppError("You do not have enough balance");
+    }
 
     let transactionCategory = await categoryRepository.findOne({
       where: {
